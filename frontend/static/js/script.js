61d5f47cdc6c5152ef9733a6faf9a343a4e980cf
Balazs Gergo <gergobalazs98@gmail.com>
2023-12-08 10:02:58 +0100

    friday

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -10,7 +10,6 @@ const formComponent = () => `
   </form>
 `
 
-rootElement.insertAdjacentHTML("beforeend", formComponent())
 
 const formElement = document.querySelector('form')
 formElement.addEventListener('submit', (event) => {
@@ -32,3 +31,40 @@ formElement.addEventListener('submit', (event) => {
     .then(res => res.json())
     .then(resJson => console.log(resJson))
 })
+
+const userCardComponent = () => {
+  `
+  <div>
+    <h3>`${userdata.id}`</h3>
+  </div>
+  `
+}
+
+
+const init = () => {
+  
+  const formElement = document.querySelector('form')
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const userName = document.querySelector(`input[name="name"]`).value
+  const userPassword = document.querySelector(`input[name="password"]`).value
+
+  fetch('/users/new-user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: userName,
+      password: userPassword
+    })
+  })
+    .then(res => res.json())
+    .then(resJson => console.log(resJson))
+})
+
+
+} 
+
+init()
